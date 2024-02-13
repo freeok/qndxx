47da73319c5b8deed68b31c8c69ec72aa4c4437a
freeok <51998152+freeok@users.noreply.github.com>
2024-02-14 05:06:01 +0800

    templates 目录结构调整

--- src/main/java/work/pcdd/qndxx/common/interceptor/StaticResourcesInterceptor.java
+++ src/main/java/work/pcdd/qndxx/common/interceptor/StaticResourcesInterceptor.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class StaticResourcesInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         return false;
     }
 
